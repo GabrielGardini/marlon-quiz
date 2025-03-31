f5223bf57bf8b02e3e3e4469a805c887354210f5
Gabriel Gardini <gabrielgardini1@gmail.com>
2025-03-31 14:24:36 -0300

    first commit

---
+++ marlon-quiz/marlon-quiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  marlon-quiz
+//
+//  Created by Aluno 15 on 31/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ marlon-quiz/marlon-quiz/marlon_quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  marlon_quizApp.swift
+//  marlon-quiz
+//
+//  Created by Aluno 15 on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct marlon_quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
