b83786d9fc9d32e99cc3743ebbca5ec78c39e889
Gabriel Gardini <gabrielgardini1@gmail.com>
2025-03-31 15:29:56 -0300

    oie soife

--- marlon-quiz/marlon-quiz/ContentView.swift
+++ marlon-quiz/marlon-quiz/ContentView.swift
@@ -1,16 +1,87 @@
-//
-//  ContentView.swift
-//  marlon-quiz
-//
-//  Created by Aluno 15 on 31/03/25.
-//
-
 import SwiftUI
 
+// Classe para armazenar a variável global
+class GlobalData: ObservableObject {
+    @Published var selectedValue: String = "Nenhum"
+}
+
 struct ContentView: View {
+    @StateObject var globalData = GlobalData() // Criamos a instância da classe GlobalData
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Tela Inicial")
+                    .font(.largeTitle)
+                    .padding()
+
+                Text("Valor Selecionado: \(globalData.selectedValue)")
+                    .font(.headline)
+                    .padding()
+
+                NavigationLink(destination: SecondView().environmentObject(globalData)) {
+                    Text("Ir para a Segunda Tela")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+        }
+    }
+}
+
+struct SecondView: View {
+    @EnvironmentObject var globalData: GlobalData
+
+    var body: some View {
+        VStack {
+            Text("Segunda Tela")
+                .font(.largeTitle)
+                .padding()
+
+            Text("Valor Atual: \(globalData.selectedValue)")
+                .font(.headline)
+                .padding()
+
+            NavigationLink(destination: ThirdView().environmentObject(globalData)) {
+                Text("Botão 1 (Vai para a Terceira Tela)")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .simultaneousGesture(TapGesture().onEnded {
+                globalData.selectedValue = "Botão 1 clicado"
+            })
+
+            NavigationLink(destination: ThirdView().environmentObject(globalData)) {
+                Text("Botão 2 (Vai para a Terceira Tela)")
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .simultaneousGesture(TapGesture().onEnded {
+                globalData.selectedValue = "Botão 2 clicado"
+            })
+        }
+    }
+}
+
+struct ThirdView: View {
+    @EnvironmentObject var globalData: GlobalData
+
+    var body: some View {
+        VStack {
+            Text("Terceira Tela")
+                .font(.largeTitle)
+                .padding()
+
+            Text("Valor recebido: \(globalData.selectedValue)")
+                .font(.headline)
+                .padding()
+        }
     }
 }
 
